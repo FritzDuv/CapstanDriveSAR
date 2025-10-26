5b4b8c44c23b0deed18ab5e44a035079503933c2
Eghosa Ohenhen <coolaschocolate@gmail.com>
2025-10-25 20:28:03 -0400

    removed build

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Fritz/source/repos/CapstanDriveSAR
-# Build directory: C:/Users/Fritz/source/repos/CapstanDriveSAR/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/Fritz/source/repos/CapstanDriveSAR/build/mytest[1]_include.cmake")
-subdirs("_deps/googletest-build")
--- build/mytest[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "C:/Users/Fritz/source/repos/CapstanDriveSAR/build/mytest[1]_tests.cmake")
-  include("C:/Users/Fritz/source/repos/CapstanDriveSAR/build/mytest[1]_tests.cmake")
-else()
-  add_test(mytest_NOT_BUILT mytest_NOT_BUILT)
-endif()
